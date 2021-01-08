b4422279e53b40cda6497464e479ae5652394ef6
DUCANCHU3E86\macintoshhd <chuducan.dev@gmail.com>
2021-01-09 01:20:32 +0700

    Game balance changes

--- src/CGAME.cpp
+++ src/CGAME.cpp
@@ -245,7 +245,7 @@ void CGAME::updateSound() {
 }
 
 float logLevel(int level) {
-    return level > 3 ? 0.75 * log(level * 0.5) / log(2) : 1;
+    return level > 2 ? 0.65 * log(level * 0.5) / log(2) : 1;
 }
 
 void CGAME::updateLanes() {
